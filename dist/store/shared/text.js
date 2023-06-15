c8eb3f155670d760614ba5e8433c7bf5f28199c9
Kyle Kumar <62655757+kkumarcodes@users.noreply.github.com>
2023-06-14 20:47:29 -0400

    Delete dist/store directory

--- dist/store/shared/text.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-var _settings = require('../client/settings');
-
-var _settings2 = _interopRequireDefault(_settings);
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
-
-module.exports = require('../../../locales/store/' + _settings2.default.language + '.json');
