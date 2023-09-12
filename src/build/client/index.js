2487a7ddc6dec2825d4482630648a8d770579395
admin <kkumar.codes@gmail.com>
2023-09-11 23:21:02 -0400

    update build

---
+++ src/build/client/index.js
@@ -0,0 +1,24 @@
+const webpack = require('webpack');
+const {
+  createConfigClient
+} = require('../../../../lib/webpack/prod/createConfigClient');
+
+module.exports.buildClient = async function buildClient(routes) {
+  const config = createConfigClient(routes);
+  const compiler = webpack(config);
+
+  return new Promise((resolve, reject) => {
+    compiler.run((err, stats) => {
+      if (err || stats.hasErrors()) {
+        console.log(
+          stats.toString({
+            errorDetails: true,
+            warnings: true
+          })
+        );
+        reject(err);
+      }
+      resolve(stats);
+    });
+  });
+};
---
+++ src/build/complie.js
@@ -0,0 +1,29 @@
+const { webpack } = require('webpack');
+const {
+  createConfigClient
+} = require('../lib/webpack/prod/createConfigClient');
+const {
+  createConfigServer
+} = require('../lib/webpack/prod/createConfigServer');
+
+module.exports.compile = async function compile(routes) {
+  const config = [createConfigClient(routes), createConfigServer(routes)];
+
+  const compiler = webpack(config);
+
+  return new Promise((resolve, reject) => {
+    compiler.run((err, stats) => {
+      if (err || stats.hasErrors()) {
+        console.log(err);
+        console.log(
+          stats.toString({
+            errorDetails: true,
+            warnings: true
+          })
+        );
+        reject(err);
+      }
+      resolve(stats);
+    });
+  });
+};
